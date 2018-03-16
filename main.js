dacb76173b2c7af1b629c91b7d747b5ba4011629
SamDino <samuel.cas.96@gmail.com>
2018-03-16 11:04:48 -0600

    Found the bbox for every searched place

--- main.js
+++ main.js
@@ -18,38 +18,101 @@ function initMap() {
 
 	}	
 
-function showEarthquakes(jsonObj) {
-	
-	var earth = jsonObj['earthquakes'];
-	// console.log(earth.length);
-	for (i = 0; i < earth.length; i++) {
-		addMarker({lat:earth[i].lat, lng:earth[i].lng}, earth[i].datetime, earth[i].magnitude, earth[i].depth, earth[i].src);
+	function showEarthquakes(jsonObj) {
+		
+		var earth = jsonObj['earthquakes'];
+		// console.log(earth.length);
+		for (i = 0; i < earth.length; i++) {
+			// addMarker({lat:earth[i].lat, lng:earth[i].lng}, earth[i].datetime, earth[i].magnitude, earth[i].depth, earth[i].src);
+		}
+		// console.log(longi,"-",latit);
 	}
-	// console.log(longi,"-",latit);
-}
-	var infowindow = new google.maps.InfoWindow();
-	var uluru = {lat: 38.322, lng: 142.369};
-       	var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 4,
-			center: uluru
+
+	  var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13,
+          mapTypeId: 'roadmap'
         });
 
-    function addMarker(coords, date, mag, depth, su){
-     		marker = new google.maps.Marker({
-       	position: coords,
-       	map: map,
-       	title: date,
-       	icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: mag
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('place-search');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
           }
-     	});
 
-  		google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent('<b>Date: </b>' + date + '<br>' +'<b>The magnitude was: </b>' + mag + '<br>' + '<b>Depth: </b>' + depth + ' km' + '<br>' + '<b>Source: </b>' + su);
-        infowindow.open(map, this);
-      });
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
 
-    }
-}
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          console.log(bounds);
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
+      }
+
+
+
+	// function addMarker(coords, date, mag, depth, su){
+ //  		marker = new google.maps.Marker({
+ //    	position: coords,
+ //    	map: map,
+ //    	title: date,
+ //    	icon: {
+ //         path: google.maps.SymbolPath.CIRCLE,
+ //         scale: mag
+ //       }
+ //  	});
+
+	// 	google.maps.event.addListener(marker, 'click', function() {
+	// 	  infowindow.setContent('<b>Date: </b>' + date + '<br>' +'<b>The magnitude was: </b>' + mag + '<br>' + '<b>Depth: </b>' + depth + ' km' + '<br>' + '<b>Source: </b>' + su);
+	// 	  infowindow.open(map, this);
+	// 	});
+
+ //   }
+
 
