fe1ecb389d613286278e0d69d5e8f969fb8728ce
SamDino <samuel.cas.96@gmail.com>
2018-03-15 20:45:04 -0600

    multiple markers

--- main.js
+++ main.js
@@ -10,98 +10,65 @@ request.send();
 
 request.onload = function() {
 
-	  var earthquakesList = request.response;
-	  showEarthquakes(earthquakesList);
+	var earthquakesList = request.response;
+	showEarthquakes(earthquakesList);
 
-	}	
+}	
 
 function showEarthquakes(jsonObj) {
 	
 	var earth = jsonObj['earthquakes'];
 	// console.log(earth.length);
-	for (var i = 0; i < earth.length; i++) {
+	for (i = 0; i < earth.length; i++) {
 		count++;
 		latit.push(earth[i].lng);
 		longi.push(earth[i].lat);
 	}
 	// console.log(longi,"-",latit);
-
 }
 
 function initMap() {
 
-	var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.866, lng: 151.196},
-          zoom: 15
-	});
+	var uluru = {lat: 38.322, lng: 142.369};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
 
-	// console.log(longi, "-", latit);
-	// newlat = Math.floor((Math.random() * 100) + 1);
-	// newlng = Math.floor((Math.random() * 100) + 1);
+    var infowindow = new google.maps.InfoWindow();
+    var marker;
+
+    for (var i = 0; i < 9; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(Math.random(), Math.random()),
+        map: map,
+        title: "click me " + i
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent('Hello');
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+
+    }
+    // console.log(longi, "-", latit);
 
 	// for (var i = 0; i < 9; i++) {
-	// 	var myLatlng = new google.maps.LatLng(, );
 
-	// 	var newMarker = new google.maps.Marker({
-	// 		position: myLatlng,
+	// 	var marker = new google.maps.Marker({
+	// 		position: new google.maps.LatLng(latit[i], long[i]),
 	// 		map = map,
 	// 		title: 'earthquakes'
 	// 	});
 	
 	// }
 
-	var input = document.getElementById('place-search');
-    var searchBox = new google.maps.places.SearchBox(input);
-
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
-    });
-
-    var markers = [];
-    
-    searchBox.addListener('places_changed', function() {
-      var places = searchBox.getPlaces();
-
-      if (places.length == 0) {
-        return;
-      }
-
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = [];
-
-      var bounds = new google.maps.LatLngBounds();
-
-      places.forEach(function(place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        var icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        markers.push(new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        }));
-
-        if (place.geometry.viewport) {
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
+    // for (i = 0; i < locations.length; i++) {  
+    //   marker = new google.maps.Marker({
+    //     position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+    //     map: map
+    //   });
   }
 
