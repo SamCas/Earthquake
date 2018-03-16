23150e4abef528ec3bee043e3da0b292d7b85d7f
SamDino <samuel.cas.96@gmail.com>
2018-03-15 21:53:40 -0600

    Best one yet.

--- main.js
+++ main.js
@@ -23,18 +23,18 @@ function showEarthquakes(jsonObj) {
 	var earth = jsonObj['earthquakes'];
 	// console.log(earth.length);
 	for (i = 0; i < earth.length; i++) {
-		addMarker({lat:earth[i].lat, lng:earth[i].lng}, earth[i].datetime, earth[i].magnitude);
+		addMarker({lat:earth[i].lat, lng:earth[i].lng}, earth[i].datetime, earth[i].magnitude, earth[i].depth, earth[i].src);
 	}
 	// console.log(longi,"-",latit);
 }
-
+	var infowindow = new google.maps.InfoWindow();
 	var uluru = {lat: 38.322, lng: 142.369};
        	var map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 4,
 			center: uluru
         });
 
-    function addMarker(coords, date, mag){
+    function addMarker(coords, date, mag, depth, su){
      		marker = new google.maps.Marker({
        	position: coords,
        	map: map,
@@ -44,6 +44,12 @@ function showEarthquakes(jsonObj) {
             scale: mag
           }
      	});
+
+  		google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent('<b>Date: </b>' + date + '<br>' +'<b>The magnitude was: </b>' + mag + '<br>' + '<b>Depth: </b>' + depth + ' km' + '<br>' + '<b>Source: </b>' + su);
+        infowindow.open(map, this);
+      });
+
     }
 }
 
